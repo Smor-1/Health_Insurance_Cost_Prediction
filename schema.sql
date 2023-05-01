58cd7ecb4711040a4af8132aa3913d80238c49e3
Pazz0 <massimoscanga@hotmail.com>
2023-05-01 11:18:43 -0400

    updated info columns to include smoker

---
+++ schema.sql
@@ -0,0 +1,14 @@
+CREATE TABLE info (
+    ID SERIAL PRIMARY KEY,
+    age INTEGER NOT NULL,
+    sex VARCHAR(10) NOT NULL,
+    bmi FLOAT NOT NULL,
+    children INTEGER NOT NULL,
+    smoker VARCHAR(3) NOT NULL
+);
+
+CREATE TABLE charges (
+    ID VARCHAR(10) PRIMARY KEY,
+    region VARCHAR(20) NOT NULL,
+    charges FLOAT NOT NULL
+);
