770d56e3fa4d6b8f5e24efae56ee73e208408f9b
Pazz0 <massimoscanga@hotmail.com>
2023-05-17 10:30:04 -0400

    troublehooting file and update to app

--- app.py
+++ app.py
@@ -11,34 +11,41 @@ model = joblib.load('finalized_model.sav')
 
 @app.route('/predict', methods=['POST', 'OPTIONS'])
 def predict():
-    if request.method == 'OPTIONS':
-        # CORS preflight
-        response = jsonify()
-    else:
-        # Read the data from the JSON body of the request
-        data = request.get_json()
-        age = data['age']
-        bmi = data['bmi']
-        smoker = data['smoker']
-        # Prepare the feature vector for prediction
-        features = [[age, bmi, smoker]]
+    #test
+    response = jsonify(message='Hello, CORS is enabled!')
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    return response
 
-        # Make a prediction
-        prediction = model.predict(features)
+    # OG code
+   #if request.method == 'OPTIONS':
+   #    # CORS preflight
+   #    response = jsonify()
+   #else:
+   #    # Read the data from the JSON body of the request
+   #    data = request.get_json()
+   #    age = data['age']
+   #    bmi = data['bmi']
+   #    smoker = data['smoker']
+   #    # Prepare the feature vector for prediction
+   #    features = [[age, bmi, smoker]]
 
-        # Prepare the prediction for JSON serialization
-        insurance_cost = prediction[0]
+   #    # Make a prediction
+   #    prediction = model.predict(features)
 
-        # CORS headers
-        response = jsonify(insurance_cost=insurance_cost)
-        response.headers.add('Access-Control-Allow-Origin', '*')
-        response.headers.add('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
-        response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+   #    # Prepare the prediction for JSON serialization
+   #    insurance_cost = prediction[0]
 
-        return response
+   #    # CORS headers
+   #    response = jsonify(insurance_cost=insurance_cost)
+   #    response.headers.add('Access-Control-Allow-Origin', '*')
+   #    response.headers.add('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+   #    response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
 
-        #return jsonify(insurance_cost=insurance_cost)
+   #    return response
+
+   #    #return jsonify(insurance_cost=insurance_cost)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5500)
-
