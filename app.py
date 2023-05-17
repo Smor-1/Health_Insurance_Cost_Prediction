38103eb45d3acecdbd51d3903c051e14d1c198e8
Pazz0 <massimoscanga@hotmail.com>
2023-05-17 14:20:36 -0400

    updated app py and main js ports to 5001

--- app.py
+++ app.py
@@ -1,38 +1,31 @@
-from flask import Flask, jsonify
-import joblib
+from flask import Flask, jsonify, request
 from flask_cors import CORS
+import joblib
 
 app = Flask(__name__)
-# enable CORS
-CORS(app)
+CORS(app, supports_credentials=True)
 
 # Load the model
 model = joblib.load('testRF_model.sav')
 
 @app.route('/predict', methods=['POST', 'OPTIONS'])
 def predict():
-   if request.method == 'OPTIONS':
-       # CORS preflight
-       response = jsonify()
-   else:
-       # Read the data from the JSON body of the request
-       data = request.get_json()
-       age = data['age']
-       bmi = data['bmi']
-       smoker = data['smoker']
-       # Prepare the feature vector for prediction
-       features = [[age, bmi, smoker]]
-       # Make a prediction
-       prediction = model.predict(features)
-       # Prepare the prediction for JSON serialization
-       insurance_cost = prediction[0]
-       # CORS headers
-       response = jsonify(insurance_cost=insurance_cost)
-       response.headers.add('Access-Control-Allow-Origin', '*')
-       response.headers.add('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
-       response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-       return response
-       #return jsonify(insurance_cost=insurance_cost)
+    if request.method == 'OPTIONS':
+        return {}
+    else:
+        # Read the data from the JSON body of the request
+        data = request.get_json()
+        age = data['age']
+        bmi = data['bmi']
+        smoker = data['smoker']
+        # Prepare the feature vector for prediction
+        features = [[age, bmi, smoker]]
+        # Make a prediction
+        prediction = model.predict(features)
+        # Prepare the prediction for JSON serialization
+        insurance_cost = prediction[0]
+        response = jsonify(insurance_cost=insurance_cost)
+        return response
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5500)
+    app.run(debug=True, port=5001)
